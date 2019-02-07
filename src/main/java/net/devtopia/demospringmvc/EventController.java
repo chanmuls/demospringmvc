366d0a504703c40c18370d273d76d407166cbf58
Aiden <hello@devtopia.net>
2019-02-07 22:03:50 +0900

    requestMapping -> getMapping

--- src/main/java/net/devtopia/demospringmvc/EventController.java
+++ src/main/java/net/devtopia/demospringmvc/EventController.java
@@ -2,12 +2,13 @@ package net.devtopia.demospringmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventController {
-    @RequestMapping(value = "/events", method = RequestMethod.GET)
+    @GetMapping("/events")
     public String events(Model model) {
         return "events";
     }
